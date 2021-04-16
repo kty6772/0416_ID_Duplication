d9ee8d01ceb0b52108d5992cd9a42cba18c5d1f2
kty6772 <kty6777@naver.com>
2021-04-16 12:07:42 +0900

    0416_ID_Duplication

--- script/script.js
+++ script/script.js
@@ -1,3 +1,28 @@
 $(function(){
+    $("button[type=button]").click(function(){
+        var inputUID = $("#uID").val().trim(); // form 화면에서 입력한 ID
+
+
+       var regIDList = ["test01", "guest", "member03"]; // 가입된 ID를 의미
+        // regIDList 라는 배열은 인덱스 번호가 0, 1 , 2로 구성됨
+
+        var idUseChk = true;
+
+
+        $.each(regIDList, function(i, v){
+            if(inputUID == v){
+                idUseChk = false;
+               }
+        });
+
+    if(idUseChk){
+        alert("사용가능");
+    }else{
+        alert("사용중인 ID입니다.")
+    }
+
+    });
+
+
 
 });
